3e8cc42926303cb7f6860eabb64a5cb7378cc352
Jose Carlos Garcia <jcgarcia@iastate.edu>
2022-11-16 01:02:19 -0600

    added additional files

--- app/src/main/java/com/example/cpre388/cuisine/Activities/reservation_activity.java
+++ app/src/main/java/com/example/cpre388/cuisine/Activities/reservation_activity.java
@@ -1,8 +1,17 @@
 package com.example.cpre388.cuisine.Activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cpre388.cuisine.R;
 
 public class reservation_activity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 }
