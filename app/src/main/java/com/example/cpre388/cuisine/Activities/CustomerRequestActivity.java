1bba93ef4071c89cf077d53c5195d36665ef4c1d
WINSLEY GOH <shiyong@iastate.edu>
2022-11-23 16:52:58 -0600

    add customer request

---
+++ app/src/main/java/com/example/cpre388/cuisine/Activities/CustomerRequestActivity.java
@@ -0,0 +1,16 @@
+package com.example.cpre388.cuisine.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cpre388.cuisine.R;
+
+public class CustomerRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_request);
+    }
+}
--- app/src/main/java/com/example/cpre388/cuisine/Activities/activity_customer_main.java
+++ app/src/main/java/com/example/cpre388/cuisine/Activities/activity_customer_main.java
@@ -1,8 +1,12 @@
 package com.example.cpre388.cuisine.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.app.ActionBar;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,15 +31,26 @@ public class activity_customer_main extends AppCompatActivity implements Navigat
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
-
+        Button btn_request = (Button)findViewById(R.id.RequestButton);
+        btn_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(activity_customer_main.this, CustomerRequestActivity.class));
+            }
+        });
         setSupportActionBar(toolbar);
-        navigationView.bringToFront();
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
                 drawerLayout,
                 toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.bringToFront();
+        navigationView.setCheckedItem(R.id.nav_home);
+
+
+
     }
 
     @Override
@@ -51,6 +66,17 @@ public class activity_customer_main extends AppCompatActivity implements Navigat
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_reciept:
+                Intent intent = new Intent(this, CustomerRecieptsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_review:
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 }
