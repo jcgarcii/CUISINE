fe2596948910f2f232f25489e69055d2188640ec
Jose Carlos Garcia <jcgarcia@iastate.edu>
2022-11-27 14:55:56 -0600

    added confirmation activity

--- app/src/main/java/com/example/cpre388/cuisine/Activities/ReservationConfirmationActivity.java
+++ app/src/main/java/com/example/cpre388/cuisine/Activities/ReservationConfirmationActivity.java
@@ -1,2 +1,16 @@
-package com.example.cpre388.cuisine.Activities;public class ReservationConfirmationActivity {
+package com.example.cpre388.cuisine.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cpre388.cuisine.R;
+
+public class ReservationConfirmationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_table);
+
+    }
 }
