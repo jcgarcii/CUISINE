0390c6e605245524088d4a6537b46d17b406cd9e
Jose Carlos Garcia <jcgarcia@iastate.edu>
2022-11-29 05:47:13 -0600

    Added welcome screen - declaring types

---
+++ app/src/main/java/com/example/cpre388/cuisine/Activities/AuthenticationActivity.java
@@ -0,0 +1,109 @@
+package com.example.cpre388.cuisine.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cpre388.cuisine.APIs.RestaurantAdapter;
+import com.example.cpre388.cuisine.R;
+import com.example.cpre388.cuisine.Util.FirebaseUtil;
+import com.example.cpre388.cuisine.ViewModels.MainActivityViewModel;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.Collections;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    private static final String TAG = "AuthenticationActivity";
+
+    private static final int RC_SIGN_IN = 9001;
+
+    private static final int LIMIT = 50;
+
+    private Toolbar mToolbar;
+    private TextView mCurrentSearchView;
+    private TextView mCurrentSortByView;
+    private RecyclerView mRestaurantsRecycler;
+    private ViewGroup mEmptyView;
+
+    private FirebaseFirestore mFirestore;
+    private Query mQuery;
+
+    private FilterDialogFragment mFilterDialog;
+    private RestaurantAdapter mAdapter;
+
+    private MainActivityViewModel mViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+        mViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Start sign in if necessary
+        if (shouldStartSignIn()) {
+            startSignIn();
+            return;
+        }
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAdapter != null) {
+            mAdapter.stopListening();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_sign_out:
+                FirebaseUtil.getAuthUI().signOut(this);
+                startSignIn();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean shouldStartSignIn() {
+        return (!mViewModel.getIsSigningIn() && FirebaseUtil.getAuth().getCurrentUser() == null);
+    }
+
+    private void startSignIn() {
+        // Sign in with FirebaseUI
+        Intent intent = FirebaseUtil.getAuthUI()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Collections.singletonList(
+                        new AuthUI.IdpConfig.EmailBuilder().build()))
+                .setIsSmartLockEnabled(false)
+                .setTheme(R.style.LoginTheme)
+                .setLogo(R.drawable.logo)
+                .build();
+
+        startActivityForResult(intent, RC_SIGN_IN);
+        mViewModel.setIsSigningIn(true);
+    }
+
+}
--- app/src/main/java/com/example/cpre388/cuisine/Activities/MainActivity.java
+++ app/src/main/java/com/example/cpre388/cuisine/Activities/MainActivity.java
@@ -2,18 +2,33 @@ package com.example.cpre388.cuisine.Activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -36,87 +51,122 @@ import com.google.firebase.firestore.Query;
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String SPLASH_SCREEN = "com.example.cpre388.cuisine.Activities.MainActivity";
 
-    private static final String TAG = "MainActivity";
+    private LinearLayout linearLayout;
+    private AnimationDrawable animationDrawable;
+    private TextView greeting1, greeting2, greeting3;
+    private VideoView videoview;
+    private MediaPlayer lefestin;
+    private MediaController.MediaPlayerControl le;
+    private ImageView mainlogo;
+    private AppCompatButton customer_btn, owner_btn;
 
-    private static final int RC_SIGN_IN = 9001;
-
-    private static final int LIMIT = 50;
-
-    private Toolbar mToolbar;
-    private TextView mCurrentSearchView;
-    private TextView mCurrentSortByView;
-    private RecyclerView mRestaurantsRecycler;
-    private ViewGroup mEmptyView;
-
-    private FirebaseFirestore mFirestore;
-    private Query mQuery;
-
-    private FilterDialogFragment mFilterDialog;
-    private RestaurantAdapter mAdapter;
-
-    private MainActivityViewModel mViewModel;
+    //Handlers for animations:
+    private Handler first_animation, sec_animation, third_animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        //Buttons:
+        customer_btn = findViewById(R.id.CUSTOMER_btn);
+        owner_btn = findViewById(R.id.RESTAURANT_btn);
+
+        //Presentation:
+        mainlogo = findViewById(R.id.welcome_activity_logo);
+        greeting1 = findViewById(R.id.greeting_1);
+        greeting1.setVisibility(View.INVISIBLE);
+        greeting2 = findViewById(R.id.greeting_2);
+        greeting2.setVisibility(View.INVISIBLE);
+        linearLayout = findViewById(R.id.splash_header_bottom);
+
+        //Background Music:
+        lefestin = MediaPlayer.create(MainActivity.this,R.raw.jaz);
+
+        //Background Video:
+        videoview = findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.bg);
+        videoview.setVideoURI(uri);
+        videoview.start();
+
+        customer_btn.setOnClickListener(this::onCustomerPressed);
+        owner_btn.setOnClickListener(this::onOwnerPressed);
+
+        //Logo and Cool text fade in:
+        mainlogo.setImageResource(R.drawable.logo);
+        first_animation = new Handler();
+        first_animation.postDelayed(first_greeting(), 2000);
+        sec_animation = new Handler();
+        sec_animation.postDelayed(second_greeting(), 4000);
+        third_animation = new Handler();
+        third_animation.postDelayed(third_greeting(), 6000);
+
+        //Loop background video:
+        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
     }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        videoview.start();
+        lefestin.start();
+    }
+
+    private Runnable first_greeting(){
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                greeting1.setVisibility(View.VISIBLE);
+            }
+        };
+        return runnable;
+    }
+
+    private Runnable second_greeting(){
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                greeting2.setVisibility(View.VISIBLE);
+            }
+        };
+        return runnable;
+    }
+
+    private Runnable third_greeting(){
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                linearLayout.setVisibility(View.VISIBLE);
+            }
+        };
+        return runnable;
+    }
+
+    private void onCustomerPressed(View view){
+        Boolean userType = false;
+        Intent customer = new Intent(this, AuthenticationActivity.class);
+        customer.putExtra(SPLASH_SCREEN, userType);
+        startActivity(customer);
+    }
+
+    private void onOwnerPressed(View view){
+        Boolean userType = true;
+        Intent owner = new Intent(this, AuthenticationActivity.class);
+        owner.putExtra(SPLASH_SCREEN, userType);
+        startActivity(owner);
+    }
+
 
     @Override
-    public void onStart() {
-        super.onStart();
-        // Start sign in if necessary
-        if (shouldStartSignIn()) {
-            startSignIn();
-            return;
-        }
-
+    protected void onPause() {
+        super.onPause();
+        lefestin.stop();
+        videoview.stopPlayback();
     }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAdapter != null) {
-            mAdapter.stopListening();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_sign_out:
-                FirebaseUtil.getAuthUI().signOut(this);
-                startSignIn();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private boolean shouldStartSignIn() {
-        return (!mViewModel.getIsSigningIn() && FirebaseUtil.getAuth().getCurrentUser() == null);
-    }
-
-    private void startSignIn() {
-        // Sign in with FirebaseUI
-        Intent intent = FirebaseUtil.getAuthUI()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(Collections.singletonList(
-                        new AuthUI.IdpConfig.EmailBuilder().build()))
-                .setIsSmartLockEnabled(false)
-                .setTheme(R.style.LoginTheme)
-                .setLogo(R.drawable.logo)
-                .build();
-
-        startActivityForResult(intent, RC_SIGN_IN);
-        mViewModel.setIsSigningIn(true);
-    }
-
 }
