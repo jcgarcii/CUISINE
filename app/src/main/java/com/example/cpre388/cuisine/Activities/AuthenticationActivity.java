fb92c30b81ce3b803d913e0d8a561755f8e10585
Jose Carlos Garcia <jcgarcia@iastate.edu>
2022-11-30 00:16:01 -0600

    completed authentication activity

--- app/src/main/java/com/example/cpre388/cuisine/Activities/AuthenticationActivity.java
+++ app/src/main/java/com/example/cpre388/cuisine/Activities/AuthenticationActivity.java
@@ -1,28 +1,22 @@
 package com.example.cpre388.cuisine.Activities;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.cpre388.cuisine.APIs.RestaurantAdapter;
 import com.example.cpre388.cuisine.R;
 import com.example.cpre388.cuisine.Util.FirebaseUtil;
 import com.example.cpre388.cuisine.ViewModels.MainActivityViewModel;
 import com.firebase.ui.auth.AuthUI;
-import com.google.firebase.analytics.FirebaseAnalytics;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -30,27 +24,15 @@ import java.util.Map;
 
 public class AuthenticationActivity extends AppCompatActivity {
     private static final String SPLASH_SCREEN = "com.example.cpre388.cuisine.Activities.MainActivity";
-    private static final String SET_TYPE = "user_type";
     private static final String TAG = "AuthenticationActivity";
 
     private static final int RC_SIGN_IN = 9001;
 
-    private static final int LIMIT = 50;
-    private FirebaseAnalytics mFirebaseAnalytics;
-
-    private Toolbar mToolbar;
-    private TextView mCurrentSearchView;
-    private TextView mCurrentSortByView;
-    private RecyclerView mRestaurantsRecycler;
-    private ViewGroup mEmptyView;
+    private ConstraintLayout constraintLayout;
     private int type_selected;
 
     private FirebaseFirestore mFirestore;
-    private Query mQuery;
     private FirebaseUser currUser;
-
-    private FilterDialogFragment mFilterDialog;
-    private RestaurantAdapter mAdapter;
 
     private MainActivityViewModel mViewModel;
 
@@ -60,19 +42,26 @@ public class AuthenticationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_authentication);
         Intent intent = getIntent();
         type_selected = intent.getExtras().getInt(SPLASH_SCREEN);
-
+        constraintLayout = findViewById(R.id.auth_layout);
         mViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
+        //Animation Stuff:
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+
+        //Store User Object onto Firestore:
         Map<String, Object> user = new HashMap<>();
-        // Initialize Firestore and the main RecyclerView
         mFirestore = FirebaseUtil.getFirestore();
         CollectionReference store = mFirestore.collection("users");
-
         if(FirebaseAuth.getInstance().getCurrentUser() != null){
             currUser = FirebaseAuth.getInstance().getCurrentUser();
             user.put("uid", currUser.getUid());
             user.put("type", type_selected);
             store.add(user);
+            //launch next activity based on user
+            nextActivity(type_selected);
         }
     }
 
@@ -86,29 +75,14 @@ public class AuthenticationActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAdapter != null) {
-            mAdapter.stopListening();
+    private void nextActivity(int t){
+        if(t == 1){
+            //Intent owner = new Intent(this)
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_sign_out:
-                FirebaseUtil.getAuthUI().signOut(this);
-                startSignIn();
-                break;
+        else {
+            Intent customer = new Intent(this, activity_customer_main.class);
+            startActivity(customer);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     private boolean shouldStartSignIn() {
