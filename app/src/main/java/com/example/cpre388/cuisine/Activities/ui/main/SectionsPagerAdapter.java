5d88e228c09fc66b7e9945573ec1450b17166e46
Jose Carlos Garcia <jcgarcia@iastate.edu>
2022-12-03 04:36:20 -0600

    todo: finish owner fragments

--- app/src/main/java/com/example/cpre388/cuisine/Activities/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/cpre388/cuisine/Activities/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.example.cpre388.cuisine.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3 };
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -25,11 +25,20 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
         mContext = context;
     }
 
+
     @Override
     public Fragment getItem(int position) {
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0: fragment = new ReservationFragment(); break;
+            case 1: fragment = new RequestFragment();break;
+            case 2: fragment = new BillingFragment();break;
+            }
+
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        return fragment;
     }
 
     @Nullable
@@ -40,7 +49,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        // Show 3 total pages.
+        return 3;
     }
 }
